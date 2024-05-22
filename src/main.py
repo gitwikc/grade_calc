b2c9a1cc0775582130de2bb90830163f29d97c15
Sattwik Sahu <sahusattwik@gmail.com>
2024-05-22 17:26:30 +0530

    add:minor ui changes

--- src/main.py
+++ src/main.py
@@ -26,19 +26,27 @@ def show_calc(current: float, sems_passed: int, total_sems: int = 10) -> None:
     with cols[1]:
         cpi = current * completed + remaining * (1 - completed)
         st.subheader("Estimated CPI")
-        st.write(f"Your calculated CPI is {cpi :.2f}")
+        st.metric(label="CPI", value=f"{cpi :.2f}", delta=f"{(cpi - current) :.2f}")
 
 
 def main():
-    st.header("CPI Calculator")
+    st.set_page_config(page_icon=":clipboard:", page_title="CalcMyGrade")
+    st.title("CalcMyGrade - CPI Calculator")
     current = st.slider(
         label="Current CPI", min_value=0.00, max_value=10.00, step=0.01, value=6.00
     )
     sems_passed = st.slider(
         label="Semesters Completed", min_value=1, max_value=9, step=1, value=5
     )
+    total_sems = st.slider(
+        label="Semesters Completed",
+        min_value=sems_passed + 1,
+        max_value=10,
+        step=1,
+        value=10,
+    )
 
-    show_calc(current=current, sems_passed=sems_passed, total_sems=10)
+    show_calc(current=current, sems_passed=sems_passed, total_sems=total_sems)
 
 
 if __name__ == "__main__":
